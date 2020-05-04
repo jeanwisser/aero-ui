cbbf2269fa6a2655c6a98f91f9e1ef21c7f5de9f
jean <jean.wisser@gmail.com>
2020-05-04 11:32:18 +0200

    more functions to namespace infos

--- app/models/NamespaceInfo.scala
+++ app/models/NamespaceInfo.scala
@@ -1,8 +1,6 @@
 package models
 
-import core.Logger
 import core.MapHelper
-
 import scala.util.Try
 
 final case class NamespaceInfo(
@@ -12,11 +10,32 @@ final case class NamespaceInfo(
     replicationFactor: Int,
     memoryUsedBytes: Long,
     diskUsedBytes: Option[Long],
-    MemoryTotalSize: Long,
-    DiskTotalSize: Option[Long],
+    memoryTotalSize: Long,
+    diskTotalSize: Option[Long],
     memoryFreePercent: Int,
     diskFreePercent: Option[Int]
-)
+){
+
+  def humanReadableByteCountSI(bytes: Long): String = {
+    if (-1000 < bytes && bytes < 1000) return bytes + " B"
+    val ci = Array("k", "M", "G", "T", "P")
+    @scala.annotation.tailrec
+    def loop(i: Int, b:  Double): (Int, Double) = {
+      if(b <= -999_950 || b >= 999_950) loop(i + 1, b / 1000)
+      else (i, b)
+    }
+    val (index, result) = loop(0, bytes)
+    f"${result / 1000}%1.2f ${ci(index)}B"
+  }
+
+  def getMemoryUsedBytesH = humanReadableByteCountSI(memoryUsedBytes)
+  def getMemoryTotalSizeH = humanReadableByteCountSI(memoryTotalSize)
+  def getDiskUsedBytesH = diskUsedBytes.map(d => humanReadableByteCountSI(d))
+  def getDiskTotalSizeH = diskTotalSize.map(d => humanReadableByteCountSI(d))
+  def getMemoryUsagePercentage = 100 - memoryFreePercent
+  def getDiskUsagePercentage = 100 - memoryFreePercent
+  def isUsingDisk = diskUsedBytes.isDefined && diskTotalSize.isDefined && diskFreePercent.isDefined
+}
 
 object NamespaceInfo {
   def apply(name: String, properties: String): NamespaceInfo = {
