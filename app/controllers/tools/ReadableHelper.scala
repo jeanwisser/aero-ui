6d947744cb961febc7a0d6e295f8f3e483e05764
jean <jean.wisser@gmail.com>
2020-05-13 16:27:43 +0200

    fix readable helper

--- app/controllers/tools/ReadableHelper.scala
+++ app/controllers/tools/ReadableHelper.scala
@@ -2,17 +2,18 @@ package controllers.tools
 
 object ReadableHelper {
   def toHumanReadableBytes(bytes: Long): String = {
-    if (-1000 < bytes && bytes < 1000) return s"$bytes B"
+    if (-1024 < bytes && bytes < 1024) return s"$bytes B"
     val ci = Array("k", "M", "G", "T", "P")
 
     @scala.annotation.tailrec
     def loop(i: Int, b: Double): (Int, Double) = {
-      if (b <= -999_950 || b >= 999_950) loop(i + 1, b / 1000)
+      if (b <= -999_950 || b >= 999_950) loop(i + 1, b / 1024)
       else (i, b)
     }
 
     val (index, result) = loop(0, bytes)
-    f"${result / 1000}%1.2f ${ci(index)}B"
+    val stringValue = if ((result / 1024) % 1 == 0) f"${result / 1024}%1.0f" else f"${result / 1024}%1.2f"
+    s"$stringValue ${ci(index)}B"
   }
 
   def toHumanReadableNumber(number: Long): String = {
